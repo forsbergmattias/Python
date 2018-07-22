94766a0c6382db98a0db80c389f15400b968cb48
Mattias Forsberg <forsberg.mattias@gmail.com>
2018-07-22 18:43:01 +0200

    Switched provider and started finding historical data

--- src/StockQuoteTicker.py
+++ src/StockQuoteTicker.py
@@ -1,12 +1,21 @@
+# Finding a Stock quote can be done here
+# https://www.xignite.com/product/global-real-time-stock-quote-data/#/DeveloperResources/request/GetGlobalRealTimeQuote
+# The JSON URL is
+# https://globalrealtime.xignite.com/v3/xGlobalRealTime.json/GetGlobalRealTimeQuote?IdentifierType=Symbol&Identifier=HSBAl.CHIX
+
 import json
 import urllib3
 import certifi
 
-jsonurl = 'https://globalrealtime.xignite.com/v3/xGlobalRealTime.json/GetGlobalRealTimeQuote?IdentifierType=Symbol&Identifier=HSBAl.CHIX'
+jsonurl = 'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=SEK&apikey=2N4O1OBLGVB0P50X'
+#jsonurl = 'https://globalrealtime.xignite.com/v3/xGlobalRealTime.json/GetGlobalRealTimeQuote?IdentifierType=Symbol&Identifier=HSBAl.CHIX'
 http = urllib3.PoolManager(
     cert_reqs='CERT_REQUIRED',
     ca_certs=certifi.where()
 )
-response = http.request('GET',jsonurl)
+response = http.request('GET', jsonurl)
 data = json.loads(response.data.decode('utf-8'))
-print(data.pop("Message"))
+print(data.pop("Meta Data"))
+
+#for date,rates in data.pop("Time Series (Digital Currency Daily)"):
+#    print(date + rates)
